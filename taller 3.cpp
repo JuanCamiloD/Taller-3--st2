d98981f93fb7ca893a88a03ce8d2c6edeb62238c
JuanCamiloD <37221157+JuanCamiloD@users.noreply.github.com>
2018-08-21 23:45:22 -0500

    subido
    taller 2 y 3

--- taller 3.cpp
+++ taller 3.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <math.h>
 
 struct competidor {
 	char nombre[30], club[30],sexo[15];
@@ -31,19 +32,26 @@ struct alumno{
 
 float promedio_al[100];
 int tamano;
-int base1, base2
+int base1, base2;
 int cantEstu = 0;
 
+void menu_principal();
+void comprobar( char r);
 void menu2();
 void crearEstudiante();
 void mostrarEstudiantes();
 void mejorEstudiante();
-
+void ejercicio2();
+void ejercicio_1_100();
+void ejercicio_par();
+void ejercicio_impar();
+void ejercicio_de_tablas();
+void fibonancci();  
 void ingresar();
 void categoria();
-
-
-void menu();
+void taller_2();
+void ejercicio_1();
+void menu_taller_3();
 void mostrar_array();
 void matriz_4x4();
 void matriz_ram();
@@ -53,30 +61,219 @@ void mostrar_matriz();
 int i,j,A;
 
 int main(){
-	
-	menu();
-	
+
+ 	menu_principal();
 	system("pause");
 	return 0;
 }
+void menu_principal(){
+	
+
+int opcion;
+		do{
+		
+	
+		printf("MENU \n");
+	    printf("1. Taller 2. \n");
+		printf("2.  Taller 3. \n");
+		
+		printf("0. exit \n");
+		printf("Ingrese un opcion: \n");
+		scanf("%d", &opcion);
+		switch(opcion){
+			case 1: taller_2();
+				break;
+			case 2: menu_taller_3();
+				break;	
+			case 0: exit(0);
+				break;
+			default: printf("Opcion invalida\n");
+		}
+		system("pause");
+		system("cls");
+	}while(opcion != 0);
+}
+
+void taller_2(){
+
+int opcion;
+		do{
+		
+	
+		printf("MENU TALLER 2 \n");
+	    printf("1. Presione una tecla para comprobar si es un numero. \n");
+		printf("2.  \n");
+		printf("3. Suma los numeros del 1 al 100. \n");
+		printf("4. Sumar numeros pares del 1 al 50. \n");
+		printf("5. Sumar numeros impares del 1 al 50 . \n");
+		printf("6. Serie fibonancci. \n");
+		printf("0. exit \n");
+		printf("Ingrese un opcion: \n");
+		scanf("%d", &opcion);
+		switch(opcion){
+			case 1: ejercicio_1 ();
+				break;
+			case 2:ejercicio_1_100();
+				break;
+			case 3: ejercicio_par();
+				break;
+			case 4: ejercicio_impar() ;
+				break;
+			case 5: ejercicio_de_tablas()  ;
+				break;
+			case 6: ejercicio2();
+				break;
+			case 7:fibonancci() ;
+				break;	
+			case 0: menu_principal();
+				break;
+			default: printf("Opcion invalida\n");
+		}
+		system("pause");
+		system("cls");
+	}while(opcion != 0);
+}
 
 
-void menu(){
+
+void ejercicio_1 (){
+	char leer;
+	printf("Presione una tecla: \n");
+	scanf("%c",&leer);
+ 	comprobar(leer);
+
+	printf("El programa termino\n");
+	
+	system("pause");
+
+}
+
+
+void comprobar(char r){
+	int l= int (r);
+	int a;
+	if ( l >= 48 && l <= 57 ){
+		printf("Correcto, la tecla que pulso fue un numero.\n");
+		 a = 1;
+		 
+	}
+		printf("%d\n", l);
+	if (l<48 || l>57){
+		
+			a = 0;
+			//int l= int (r);
+		printf("Vuelve a intentarlo: \n");
+	
+	
+		printf("Incorrecto, la tecla pulsada no es un numero.\n");
+		
+		r = NULL;
+		scanf("%c",&r);	
+	    comprobar(r);
+	}
+
+
+}
+void ejercicio2(){
+   int i;
+   int fact=1;
+   int numero;
+   printf("ingresa un numero: ");
+   scanf("%d",&numero);
+   if(numero<0) fact =0;
+   else if(numero==0) fact=1;
+   else{
+      for (i = 1; i <= numero; i++){
+         fact = fact*i;
+      }
+   }
+   printf("Factorial de %d = %d",numero,fact);
+   system("pause");
+   
+
+   }
+void ejercicio_1_100(){
+  int x,y,z,cont;
+
+  x=0;
+  y=1;
+ 
+  printf("0\n1\n",z);
+ 
+ while (cont<=100)
+  {
+      z=x+y;
+      printf("%d\n",z);
+      x=y;
+      y=z;
+      cont++;}
+}
+void ejercicio_par() {
+	int n=2,p=0;
+	while (n<=50){
+		p=n+p;
+		n+=2;
+		printf("suma= %d\n",p);
+	}
+	
+}
+void ejercicio_impar() {
+	int n=1,p=0;
+	while (n<=50){
+		p=n+p;
+		n+=2;
+		printf("suma= %d\n",p);
+	}
+	
+}
+
+void ejercicio_de_tablas() {
+	int i,resultado,n;
+	printf("Ingrese el numero para ver su tabla de multiplicar: \n");
+	scanf("%d",&n);
+	for(i=1;i<=10;i++){
+    resultado= n*i;
+	printf("%d * %d = %d\n",n,i, resultado);	
+	}
+	system("pause");
+	
+}
+void fibonancci(){
+	
+  int x,y,z,cont;
+
+  x=0;
+  y=1;
+ 
+  printf("0\n1\n");
+ 
+  for (cont=1;cont<=20;cont=cont+1)
+  {
+      z=x+y;
+      printf("%d\n",z);
+      x=y;
+      y=z;
+  }
+ 
+  system("PAUSE");     
+  
+}
+void menu_taller_3(){
 		int opcion;
 		do{
 		
 	
-		printf("MENU\n");
-	    printf("1. Multiplicar dos valores flotantes. \n");
-		printf("2. El cuadrado de un numero. \n");
+		printf("MENU \n");
+	    printf("1. Mostrar arreglo. \n");
+		printf("2. Matriz de unos y ceros. \n");
 		printf("3. Matriz 4x4 \n");
 		printf("4. Mostrar la parte fraccionaria de un numero. \n");
-		printf("5. El valor maximo de 3 numeros. \n");
-		printf("6. Intercambio de valores. \n");
-		printf("7. Cambio de dolares. \n");
-		printf("8. Mostrar segundos en tiempo parcial.\n");
-		printf("9. Mostrar el cambio de dias a años.\n");
-		printf("10. Suma de numeros en un vector.\n");
+		printf("5. Matriz aleatoria. \n");
+		printf("6. . \n");
+		printf("7. Matriz de numeros primos. \n");
+		printf("8. Notas de alumnos .\n");
+		printf("9. Categorias de edades en clubs.\n");
+		printf("10. .\n");
 		printf("0. exit \n");
 		printf("Ingrese un opcion: \n");
 		scanf("%d", &opcion);
@@ -101,7 +298,7 @@ void menu(){
 				break;
 			case 10: ;
 				break;	
-			case 0: exit(0);
+			case 0: menu_principal();
 				break;
 			default: printf("Opcion invalida\n");
 		}
